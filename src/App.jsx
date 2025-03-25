7d47926cda4efe92a65ea89c75372c7d04f7c505
Vladyslav <v.tsokota.69@gmail.com>
2025-03-25 11:55:46 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import userData from "./data/userData.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
+export const App = () => {
+    return (
+        <>
+            <Profile username={userData.username} tag={userData.tag} location={userData.location} image={userData.avatar} stats={userData.stats} />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </>
+    );
+};
