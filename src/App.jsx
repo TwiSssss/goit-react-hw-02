1cbef91b059b067f5d4ad0f52c38a21bba3e39d2
Vlad Tsokota <v.tsokota.69@gmail.com>
2025-03-26 09:48:34 +0200

    rrrr

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import userData from "./data/userData.json";
-import friends from "./data/friends.json";
-import transactions from "./data/transactions.json";
+import { useState, useEffect } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "./components/Notification/Notification";
 
 export const App = () => {
+    const saveClick = JSON.parse(localStorage.getItem("feedback"));
+    const [feedback, setClick] = useState(saveClick || { good: 0, neutral: 0, bad: 0 });
+    useEffect(() => {
+        localStorage.setItem("feedback", JSON.stringify(feedback));
+    }, [feedback]);
+    const updateFeedback = feedbackType => {
+        setClick ({ ...feedback, [feedbackType]: feedback[feedbackType] + 1 });
+    };
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+    const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+    const resetFeedback = () => {
+        setClick({ good: 0, neutral: 0, bad: 0 });
+    };
     return (
         <>
-            <Profile username={userData.username} tag={userData.tag} location={userData.location} image={userData.avatar} stats={userData.stats} />
-            <FriendList friends={friends} />
-            <TransactionHistory items={transactions} />
+            <Description />
+            <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
+            {totalFeedback > 0 ? <Feedback feedback={feedback} positiveFeedback={positiveFeedback} totalFeedback={totalFeedback} /> : <Notification />}
         </>
     );
 };
